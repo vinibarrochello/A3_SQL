2de46fda05e211190cd7a8e9d9f14f52a2bb311e
vinibarrochello <71726431+vinibarrochello@users.noreply.github.com>
2022-06-09 19:06:06 -0300

    Update Olimpiadas.sql
    atualizar

--- Olimpiadas.sql
+++ Olimpiadas.sql
@@ -1,27 +1,62 @@
-CREATE DATABASE IF NOT EXISTS OlimpiadaSistema;
-
-USE OlimpiadaSistema;
-
-CREATE TABLE IF NOT EXISTS Competidor(
-	CompetidorId int AUTO_INCREMENT NOT NULL,
-    CompetidorNome varchar(255) NOT NULL,
-    PRIMARY KEY(CompetidorId)
+create table Usuario(
+	UserID int not null IDENTITY,
+	UserName varchar(20) NOT NULL,
+	UserPwd varchar(64) NOT NULL,
+	UserSalt varchar(64) NOT NULL,
+	UserType varchar(15),
+	PRIMARY KEY (UserID)
 );
 
--- Para evitar problemas no BD, as imagens são guardadas em um sistema de gerenciamento de arquivos e apenas o caminho da imagem é salvo no BD
-CREATE TABLE IF NOT EXISTS Modalidade(
-	ModalidadeId int AUTO_INCREMENT NOT NULL,
-    ModalidadeNome varchar(255) NOT NULL,
-    ModalidadeImagem text,
-    PRIMARY KEY(ModalidadeId)
+create table Atleta(
+	AtlId int not null IDENTITY,
+	AtlNome varchar(100),
+	AtlNacionalidade varchar(30),
+	AtlAltura int,
+	AtlPeso decimal(5,2),
+	AtlDtaNasc date,
+	AtlUserID int,
+	PRIMARY KEY (AtlId),
+	CONSTRAINT FK_AtlID FOREIGN KEY (AtlUserID) REFERENCES Usuario(UserID)
 );
 
--- Essa tabela é responsável pela relação entre competidores e a modalidade, permitindo assim que um competidor participe de mais de uma modalidade
-CREATE TABLE IF NOT EXISTS ModalidadeResultado(
-    CompetidorId int NOT NULL,
-    ModalidadeId int NOT NULL,
-    CompetidorNota float NOT NULL,
-    CONSTRAINT PK_CompetidorModalidade PRIMARY KEY (CompetidorId,ModalidadeId),
-	FOREIGN KEY (CompetidorId) REFERENCES Competidor(CompetidorId),
-    FOREIGN KEY (ModalidadeId) REFERENCES Modalidade(ModalidadeId)
+create table Equipe(
+	EquipeID int NOT NULL IDENTITY,
+	EquipeNome varchar(50),
+	EquipeTreinador varchar(50),
+	EquipeUserID int,
+	PRIMARY KEY (EquipeID),
+	CONSTRAINT FK_EquipeID FOREIGN KEY (EquipeUserID) REFERENCES Usuario(UserID)
+);
+
+create table EquipeAtleta(
+	EquipeID int NOT NULL,
+	AtlId int NOT NULL,
+	CONSTRAINT FK_EquipeAtlID FOREIGN KEY (EquipeID) REFERENCES Equipe(EquipeID),
+	CONSTRAINT FK_AtlEquipeID FOREIGN KEY (AtlId) REFERENCES Atleta(AtlId),
+	PRIMARY KEY (EquipeID, AtlId)
+);
+
+create table Modalidade(
+	ModId INT NOT NULL IDENTITY,
+	ModNome varchar(100) NOT NULL,
+	ModDesc varchar(200),
+	ModImagePath text,
+	PRIMARY KEY (ModId)
+);
+
+create table Evento(
+	EventoID int NOT NULL IDENTITY,
+	EventoData date,
+	ModId int,
+	CONSTRAINT FK_EventoID FOREIGN KEY (ModId) REFERENCES Modalidade(ModId),
+	PRIMARY KEY (EventoID)
+);
+
+create table Registro(
+	RegistroID int NOT NULL IDENTITY,
+	EventoID int NOT NULL,
+	EquipeID int NOT NULL,
+	Pontuacao varchar(50)
+	CONSTRAINT FK_EquipeReg FOREIGN KEY (EquipeID) REFERENCES Equipe(EquipeID),
+	CONSTRAINT FK_EventoReg FOREIGN KEY (EventoID) REFERENCES Evento(EventoID),
 );
